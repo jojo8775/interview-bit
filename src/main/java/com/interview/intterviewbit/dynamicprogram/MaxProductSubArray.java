05f62a0fc60e723873a749cde38d03a6159236e3
jojo <chiranjeeb.nandy@yahoo.com>
2016-05-30 22:57:13 -0500

    Added min jump

---
+++ src/main/java/com/interview/intterviewbit/dynamicprogram/MaxProductSubArray.java
@@ -0,0 +1,51 @@
+package com.interview.intterviewbit.dynamicprogram;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MaxProductSubArray
+{
+	public int find(List<Integer> a)
+	{
+		int[] min = new int[a.size()], max = new int[a.size()];
+		min[0] = a.get(0);
+		max[0] = a.get(0);
+		
+		int result = max[0];
+		
+		for(int i=1; i<a.size(); i++){
+			if(a.get(i) > 0){
+				max[i] = Math.max(a.get(i), max[i-1] * a.get(i));
+				min[i] = Math.min(a.get(i), min[i-1] * a.get(i));
+			}
+			else{
+				max[i] = Math.max(a.get(i), min[i-1] * a.get(i));
+				min[i] = Math.min(a.get(i), max[i-1] * a.get(i));
+			}
+			
+			result = Math.max(result, max[i]);
+		}
+		
+		return result;
+	}
+
+	public static void main(String[] args)
+	{
+		// int r = new MaxProductSubArray().find(Arrays.asList(2,3,-2,4));
+		int r = new MaxProductSubArray().find(Arrays.asList(0, 0, 0, -3, -2, 0, 1, 0, 0, 0, 0, 0, -2, 0, 0, 0, 3, 3, 0,
+				0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, -2, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 3, 0, 0, 0, 3,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -2, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0,
+				0, -3, 0, 0, 0, 0, -1, 0, 2, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, -2, 0, 1, 0, 1, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, -3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -2, 0, 0, 3, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 1, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, -3, 0, 0, 0, 0, 0, 0, 0, -1, -2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
+
+		System.out.println(r);
+	}
+}
